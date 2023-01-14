ef46276b76b12b514f3e748d80e63583e9dc961d
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-01-14 01:25:33 -0300

    criado login

--- app/src/main/java/giordanisilveirasantos/exercise/instagram/activity/LoginActivity.java
+++ app/src/main/java/giordanisilveirasantos/exercise/instagram/activity/LoginActivity.java
@@ -1,23 +1,102 @@
 package giordanisilveirasantos.exercise.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import giordanisilveirasantos.exercise.instagram.R;
+import giordanisilveirasantos.exercise.instagram.helper.ConfiguracaoFirebase;
+import giordanisilveirasantos.exercise.instagram.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText campoEmail, campoSenha;
+    private Button botaoLogar;
+    private ProgressBar progressBar;
+
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        verificarUsuarioLogado();
+        inicializaComponentes();
+
+        //Fazer login do usuario
+        progressBar.setVisibility(View.GONE);
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if(!textoEmail.isEmpty()){
+                    if(!textoSenha.isEmpty()){
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin(usuario);
+
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(LoginActivity.this, "Preencha o email!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticao();
+        if(autenticacao.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
+    public void validarLogin(Usuario usuario){
+        progressBar.setVisibility(View.VISIBLE);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticao();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressBar.setVisibility(View.GONE);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Erro ao fazer login", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     public void abrirCadastro(View view){
         Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(i);
+    }
+
+    public void inicializaComponentes(){
+        campoEmail = findViewById(R.id.editLoginEmail);
+        campoSenha = findViewById(R.id.editLoginSenha);
+        botaoLogar = findViewById(R.id.buttonLogar);
+        progressBar = findViewById(R.id.progressLogin);
+        campoEmail.requestFocus();
     }
 }
